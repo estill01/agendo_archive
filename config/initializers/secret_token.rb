1da56b1eedadc8a5ed5c4676a395c3ad07fb75b0
estill01 <ehs.stillman@gmail.com>
2011-10-01 22:51:18 -0500

    boom

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agendo::Application.config.secret_token = '3c833ea3a33cc340fc4cda9d5c98cbd1ffcf05e0e18f99fe60e0b6a6fed3bc374480f1d365e0048fa441428085e7a7fc3747e7b13754503348d99d0517fe864a'
